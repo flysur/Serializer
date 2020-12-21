6d0ff480feec60c625ab0268a31318c762c8d1a8
flysur <xiaoqifeng291@sina.com>
2020-12-21 17:20:03 +0800

    Create EventDispatcher.cs
    unity3d无需进行装箱/拆箱的【事件管理器】

---
+++ Assets/Scripts/EventDispatcher.cs
@@ -0,0 +1,362 @@
+using System;
+using System.Collections.Generic;
+
+public class EventDispatcher
+
+{
+
+    public static EventDispatcher global
+
+    {
+
+        get;
+
+        private set;
+
+    }
+
+    static EventDispatcher()
+
+    {
+
+        global = new EventDispatcher();
+
+    }
+
+
+
+    private Dictionary<string, Delegate> _listeners = new Dictionary<string, Delegate>();
+
+
+
+    public void AddListener<T1, T2, T3, T4>(string evt, Action<T1, T2, T3, T4> callback)
+
+    {
+
+        AddListener(evt, (Delegate)callback);
+
+    }
+
+    public void AddListener<T1, T2, T3>(string evt, Action<T1, T2, T3> callback)
+
+    {
+
+        AddListener(evt, (Delegate)callback);
+
+    }
+
+    public void AddListener<T1, T2>(string evt, Action<T1, T2> callback)
+
+    {
+
+        AddListener(evt, (Delegate)callback);
+
+    }
+
+    public void AddListener<T>(string evt, Action<T> callback)
+
+    {
+
+        AddListener(evt, (Delegate)callback);
+
+    }
+
+    public void AddListener(string evt, Action callback)
+
+    {
+
+        AddListener(evt, (Delegate)callback);
+
+    }
+
+    public void AddListener(string evt, Delegate callback)
+
+    {
+
+        Delegate listener;
+
+        if (_listeners.TryGetValue(evt, out listener))
+
+        {
+
+            _listeners[evt] = Delegate.Combine(listener, callback);
+
+        }
+
+        else
+
+        {
+
+            _listeners[evt] = callback;
+
+        }
+
+    }
+
+
+
+    public void RemoveListener<T1, T2, T3, T4>(string evt, Action<T1, T2, T3, T4> callback)
+
+    {
+
+        RemoveListener(evt, (Delegate)callback);
+
+    }
+
+    public void RemoveListener<T1, T2, T3>(string evt, Action<T1, T2, T3> callback)
+
+    {
+
+        RemoveListener(evt, (Delegate)callback);
+
+    }
+
+    public void RemoveListener<T1, T2>(string evt, Action<T1, T2> callback)
+
+    {
+
+        RemoveListener(evt, (Delegate)callback);
+
+    }
+
+    public void RemoveListener<T>(string evt, Action<T> callback)
+
+    {
+
+        RemoveListener(evt, (Delegate)callback);
+
+    }
+
+    public void RemoveListener(string evt, Action callback)
+
+    {
+
+        RemoveListener(evt, (Delegate)callback);
+
+    }
+
+    private void RemoveListener(string evt, Delegate callback)
+
+    {
+
+        Delegate listener;
+
+        if (_listeners.TryGetValue(evt, out listener))
+
+        {
+
+            listener = Delegate.Remove(listener, callback);
+
+            if (listener == null)
+
+            {
+
+                _listeners.Remove(evt);
+
+            }
+
+            else
+
+            {
+
+                _listeners[evt] = listener;
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Dispatch<T1, T2, T3, T4>(string evt, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+
+    {
+
+        Delegate[] methods = GetMethods(evt);
+
+        if (methods != null)
+
+        {
+
+            foreach (Delegate m in methods)
+
+            {
+
+                try
+
+                {
+                    if (m.Target != null)
+                        ((Action<T1, T2, T3, T4>)m)(arg1, arg2, arg3, arg4);
+
+                }
+
+                catch (Exception e) { LogError(e); }
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Dispatch<T1, T2, T3>(string evt, T1 arg1, T2 arg2, T3 arg3)
+
+    {
+
+        Delegate[] methods = GetMethods(evt);
+
+        if (methods != null)
+
+        {
+
+            foreach (Delegate m in methods)
+
+            {
+
+                try
+
+                {
+                    if (m.Target != null)
+                        ((Action<T1, T2, T3>)m)(arg1, arg2, arg3);
+
+                }
+
+                catch (Exception e) { LogError(e); }
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Dispatch<T1, T2>(string evt, T1 arg1, T2 arg2)
+
+    {
+
+        Delegate[] methods = GetMethods(evt);
+
+        if (methods != null)
+
+        {
+
+            foreach (Delegate m in methods)
+
+            {
+
+                try
+
+                {
+                    if (m.Target != null)
+                        ((Action<T1, T2>)m)(arg1, arg2);
+
+                }
+
+                catch (Exception e) { LogError(e); }
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Dispatch<T>(string evt, T arg)
+
+    {
+
+        Delegate[] methods = GetMethods(evt);
+
+        if (methods != null)
+
+        {
+
+            foreach (Delegate m in methods)
+
+            {
+
+                try
+
+                {
+
+                    if (m.Target != null)
+                    {
+                        ((Action<T>)m)(arg);
+                    }
+
+                }
+
+                catch (Exception e) { LogError(e); }
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Dispatch(string evt)
+
+    {
+
+        Delegate[] methods = GetMethods(evt);
+
+        if (methods != null)
+
+        {
+
+            foreach (Delegate m in methods)
+
+            {
+
+                try
+
+                {
+                    if (m.Target != null)
+                        ((Action)m)();
+
+                }
+
+                catch (Exception e) { LogError(e); }
+
+            }
+
+        }
+
+    }
+
+
+
+    private Delegate[] GetMethods(string evt)
+
+    {
+
+        Delegate listener;
+
+        if (_listeners.TryGetValue(evt, out listener))
+
+        {
+
+            return listener.GetInvocationList();
+
+        }
+
+        return null;
+
+    }
+
+
+
+    private static void LogError(Exception e)
+    {
+
+        UnityEngine.Debug.LogError(e);
+
+    }
+
+}
